fc95b0a6588c8910f4f53b5c4cd07d9f96d57dec
swkim0911 <qqazws7@konkuk.ac.kr>
2024-08-06 18:46:11 +0900

    init BE

---
+++ BE/src/main/java/com/swkim/safetrip/SafetripApplication.java
@@ -0,0 +1,13 @@
+package com.swkim.safetrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetripApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/swkim/safetrip/SafetripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swkim.safetrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
