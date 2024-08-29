1bda54cced00ba89a8a1964f85eef3c1c7cd5270
swkim0911 <qqazws7@konkuk.ac.kr>
2024-08-29 13:47:29 +0900

    [#1] add: ReportService 클래스 생성 ReportService에서 report를 등록하는 함수 추가

---
+++ BE/src/main/java/com/swkim/safetrip/service/ReportService.java
@@ -0,0 +1,18 @@
+package com.swkim.safetrip.service;
+
+import com.swkim.safetrip.dto.request.ReportRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportService {
+
+    private final ReportRepository reportRepository;
+
+    public Long enroll(ReportRequest reportRequest) {
+        //todo validation
+        Report report = ReportMapper.toReport(reportRequest);
+
+        Report savedReport = reportRepository.save(report);
+        return savedReport.getId();
+    }
+}
