016248fbaf1c3c5427806c4499b31c43c7c1d16c
swkim0911 <qqazws7@konkuk.ac.kr>
2024-09-03 17:29:21 +0900

    [#1] add: ReportRepository 클래스 추가

--- BE/src/main/java/com/swkim/safetrip/service/ReportService.java
+++ BE/src/main/java/com/swkim/safetrip/service/ReportService.java
@@ -3,9 +3,12 @@ package com.swkim.safetrip.service;
 import com.swkim.safetrip.dto.request.ReportRequest;
 import com.swkim.safetrip.entity.Report;
 import com.swkim.safetrip.mapper.ReportMapper;
+import com.swkim.safetrip.repository.ReportRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ReportService {
 
     private final ReportRepository reportRepository;
