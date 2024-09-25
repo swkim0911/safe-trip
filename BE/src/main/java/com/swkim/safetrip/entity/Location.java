d79c4aff37a1b6ccdd145c92a3de12ff4141a43e
swkim0911 <qqazws7@konkuk.ac.kr>
2024-09-25 22:28:36 +0900

    [#6] add: Location 엔티티 추가

---
+++ BE/src/main/java/com/swkim/safetrip/entity/Location.java
@@ -0,0 +1,35 @@
+package com.swkim.safetrip.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "location")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @OneToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "latitude", nullable = false)
+    private Double latitude;
+
+    @Column(name = "longitude", nullable = false)
+    private Double longitude;
+}
