020d85e96cfb29f15f6b864dae2671b0c176bb4b
swkim0911 <qqazws7@konkuk.ac.kr>
2024-09-25 22:32:39 +0900

    [#6] add: Country 엔티티 생성

---
+++ BE/src/main/java/com/swkim/safetrip/entity/Country.java
@@ -0,0 +1,28 @@
+package com.swkim.safetrip.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "country")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "city_id")
+    private City capital;
+
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL) // 양방향
+    private List<City> cities;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
