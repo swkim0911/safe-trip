797f8fa119dd70052b6e2500dd9bd9e803ba2ac8
swkim0911 <qqazws7@konkuk.ac.kr>
2025-04-01 18:15:14 +0900

    [#16] add: UserRepository 인터페이스 생성

--- BE/src/main/java/com/swkim/safetrip/service/UserService.java
+++ BE/src/main/java/com/swkim/safetrip/service/UserService.java
@@ -3,6 +3,7 @@ package com.swkim.safetrip.service;
 import com.swkim.safetrip.dto.request.SignUpRequest;
 import com.swkim.safetrip.entity.User;
 import com.swkim.safetrip.mapper.UserMapper;
+import com.swkim.safetrip.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +16,8 @@ public class UserService {
     public Long enroll(SignUpRequest signUpRequest) {
 
         User user = UserMapper.toUser(signUpRequest);
-
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+        return savedUser.getId();
     }
 
 
