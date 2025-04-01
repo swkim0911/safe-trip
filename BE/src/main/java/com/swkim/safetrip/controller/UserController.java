c8100cb30793019a8d405ca12303725004936780
swkim0911 <qqazws7@konkuk.ac.kr>
2025-04-01 18:04:33 +0900

    [#16] add: SignUpRequest Dto 검증 추가

--- BE/src/main/java/com/swkim/safetrip/controller/UserController.java
+++ BE/src/main/java/com/swkim/safetrip/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.swkim.safetrip.controller;
 
+import com.swkim.safetrip.dto.request.SignUpRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +15,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public String signup(SignUpRequest SignUpRequest) {
+    public String signup(@Valid SignUpRequest SignUpRequest) {
         return "Sign up";
     }
 }
