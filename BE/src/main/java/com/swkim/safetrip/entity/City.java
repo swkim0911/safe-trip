e96352b8066ee1941c83e5ee1d2dde0a59576b2c
swkim0911 <qqazws7@konkuk.ac.kr>
2025-04-30 13:36:25 +0900

    [#20] add: City entity name 칼럼에 unique 제약 조건 추가

--- BE/src/main/java/com/swkim/safetrip/entity/City.java
+++ BE/src/main/java/com/swkim/safetrip/entity/City.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "city")
+@Table(name = "city", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "name")
+})
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class City {
