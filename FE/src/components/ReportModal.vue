d84a4ac9f00d115e16c179ce1e2bd75ea9639195
swkim0911 <qqazws7@konkuk.ac.kr>
2025-05-10 11:12:14 +0900

    [#21] add: axios 로컬 서버 통신 구현

--- FE/src/components/ReportModal.vue
+++ FE/src/components/ReportModal.vue
@@ -254,27 +254,45 @@ const submitForm = async () => {
     return;
   }
 
-  submitMessage.value = '글이 등록되었습니다.';
-  submitStatus.value = 'success';
-  resetForm();
-    // try {
-  //   const formData = new FormData()
-  //   formData.append('request', extractJsonFromForm())
+  try {
+    const formData = new FormData();
+    formData.append('request', extractJsonFromForm());
+    if (form.imageFile) {
+      formData.append('images', form.imageFile)
+    }
 
-  //   if (form.imageFile) {
-  //     formData.append('images', form.imageFile)
-  //   }
-
-  //   const response = await axios.post('http://localhost:8080/reports', formData, {
-  //     headers: {
-  //       'Content-Type': 'multipart/form-data'
-  //     }
-  //   })
-  //   console.log(response);
-
-  // } catch (error) {
+    const response = await axios.post('http://localhost:8080/reports', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
     
-  // }
+    submitMessage.value = '글이 등록되었습니다.';
+    submitStatus.value = 'success';
+    resetForm();
+
+  } catch (error) {
+    if (error.response) {
+    // 서버에서 응답을 받았지만 오류 상태 코드
+    const status = error.response.status;
+
+    if (status === 400) {
+      submitMessage.value = '입력값이 잘못되었습니다.';
+    } else if (status === 404) {
+      submitMessage.value = '요청한 API를 찾을 수 없습니다.';
+    } else if (status === 500) {
+      submitMessage.value = '서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.';
+    } else {
+      submitMessage.value = `알 수 없는 오류 (코드 ${status})`;
+    }
+
+    } else if (error.request) { // 요청이 전송되었지만 응답이 없음
+      submitMessage.value = '서버로부터 응답이 없습니다. 네트워크를 확인해주세요.';
+    } else { // 기타 에러
+      submitMessage.value = '요청 중 알 수 없는 오류가 발생했습니다.';
+    }
+    submitStatus.value = 'error';
+  }
 }
 
 const updateCharCnt = (event, type) => {
