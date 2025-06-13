5da02856ae53cf8ee2e6b975d8060114f07a7459
swkim0911 <qqazws7@konkuk.ac.kr>
2025-06-13 15:40:52 +0900

    [#40] feat: UserRepository 클래스 existsByNickname 함수 추가

--- BE/src/main/java/com/swkim/safetrip/repository/UserRepository.java
+++ BE/src/main/java/com/swkim/safetrip/repository/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
 }
