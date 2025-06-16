e2996748627764cd121b31276c78f1d3ce0637c0
swkim0911 <qqazws7@konkuk.ac.kr>
2025-06-16 15:01:05 +0900

    [#40] feat: 리프래시 토큰에 해당하는 사용자의 액세스,리프래시 토큰 재발급 메서드 구현

--- BE/src/main/java/com/swkim/safetrip/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ BE/src/main/java/com/swkim/safetrip/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -39,6 +39,10 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 
     private void checkRefreshTokenAndReIssueAccessToken(HttpServletResponse response, String refreshToken) {
         userRepository.findByRefreshToken(refreshToken)
+                .ifPresent(user -> {
+                    String reIssuedRefreshToken = reIssueRefreshToken(user);
+                    jwtService.sendAccessAndRefreshToken(response, jwtService.createAccessToken(user.getEmail()), reIssuedRefreshToken);
+                });
     }
 
     private void checkAccessTokenAndAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
