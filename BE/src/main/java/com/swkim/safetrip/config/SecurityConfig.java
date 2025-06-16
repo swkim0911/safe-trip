ce2a612b26158f0e9a62eea9369be0cbe69dbcc5
swkim0911 <qqazws7@konkuk.ac.kr>
2025-06-16 18:24:01 +0900

    [#40] feat: passwordEncoder 객체 주입 구현

--- BE/src/main/java/com/swkim/safetrip/config/SecurityConfig.java
+++ BE/src/main/java/com/swkim/safetrip/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -40,6 +42,11 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
