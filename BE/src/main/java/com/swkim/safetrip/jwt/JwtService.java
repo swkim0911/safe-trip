6a5c5e083fecdfac2d97ccc925abadd963316ddc
swkim0911 <qqazws7@konkuk.ac.kr>
2025-06-16 14:19:56 +0900

    [#40] refactor: sendRefreshToken -> sendAccessandRefreshToken 으로 메서드 변경

--- BE/src/main/java/com/swkim/safetrip/jwt/JwtService.java
+++ BE/src/main/java/com/swkim/safetrip/jwt/JwtService.java
@@ -65,14 +65,13 @@ public class JwtService {
 
     public void sendAccessToken(HttpServletResponse response, String accessToken) {
         response.setStatus(HttpServletResponse.SC_OK);
-        response.setHeader(accessHeader, accessToken);
-        log.info("Add to access token header: {}", accessToken);
+        setAccessTokenHeader(response, accessToken);
     }
 
-    public void sendRefreshToken(HttpServletResponse response, String refreshToken){
+    public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken){
         response.setStatus(HttpServletResponse.SC_OK);
-        response.setHeader(refreshHeader, refreshToken);
-        log.info("Add to refresh token header: {}", refreshToken);
+        setAccessTokenHeader(response, accessToken);
+        setRefreshTokenHeader(response, refreshToken);
     }
 
     public Optional<String> extractAccessToken(HttpServletRequest request) {
