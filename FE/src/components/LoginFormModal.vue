1480b332c2fe44dea34cfca8355bb685dc634a64
swkim0911 <qqazws7@konkuk.ac.kr>
2025-06-19 14:49:17 +0900

    [#43] feat: 로그인 모달 ui 수정

--- FE/src/components/LoginFormModal.vue
+++ FE/src/components/LoginFormModal.vue
@@ -3,26 +3,27 @@
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-md">
       <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="staticBackdropLabel">Log In</h5>
+            <h5 class="modal-title w-100 text-center" id="staticBackdropLabel">Log In</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
+
           <div class="modal-body">
             <form @submit.prevent>
               <div class="mb-3">
                 <label for="email" class="form-label">Email address</label>
-                <input type="email" id="email" v-model="email" class="form-control" required />
+                <input type="email" id="email" v-model="loginForm.email" class="form-control" required placeholder="email"/>
               </div>
 
               <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" id="password" v-model="password" class="form-control" required />
+                <input type="password" id="password" v-model="loginForm.password" class="form-control" required  placeholder="password"/>
               </div>
             </form>
             <div class="d-grid mb-2">
               <button type="button" class="btn btn-primary">Log In</button>
             </div>
             <div class="text-center">
-              <button type="button" class="btn btn-link" @click="openSignupModal">Sign Up</button>
+              <button type="button" class="btn btn-link">Sign Up</button>
             </div>
           </div>
         </div>  
@@ -30,6 +31,13 @@
   </div>
 </template>
 <script setup>
+
+import { ref, reactive } from 'vue'
+
+const loginForm = reactive({
+  email: '',
+  password: ''
+})
 
 
 </script>
