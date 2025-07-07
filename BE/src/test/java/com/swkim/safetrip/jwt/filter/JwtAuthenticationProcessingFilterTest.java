3ddddca6957b97d4df8d1082f90688077cce0e9f
swkim0911 <qqazws7@konkuk.ac.kr>
2025-07-07 17:35:18 +0900

    [#44] test: 글 등록 요청시 JWT 검증이 실행되는 단위 테스트 코드 작성

--- BE/src/test/java/com/swkim/safetrip/jwt/filter/JwtAuthenticationProcessingFilterTest.java
+++ BE/src/test/java/com/swkim/safetrip/jwt/filter/JwtAuthenticationProcessingFilterTest.java
@@ -3,6 +3,7 @@ package com.swkim.safetrip.jwt.filter;
 import com.swkim.safetrip.jwt.JwtService;
 import com.swkim.safetrip.service.UserService;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -47,5 +50,18 @@ class JwtAuthenticationProcessingFilterTest {
         // then (jwtService가 호출되지 않았고, filterChain이 동작하는지 확인)
         verify(jwtService, never()).extractAccessToken(any());
         verify(filterChain).doFilter(request, response);
+    }
+
+    @Test
+    void 글_등록_요청이라면_JWT_검증이_실행된다() throws ServletException, IOException {
+        // given
+        when(request.getRequestURI()).thenReturn("/reports");
+        when(request.getMethod()).thenReturn("POST");
+
+        // when
+        filter.doFilterInternal(request, response, filterChain);
+
+        // then (jwtService가 accessToken을 추출하는지 검증한다)
+        verify(jwtService).extractAccessToken(any());
     }
 }
