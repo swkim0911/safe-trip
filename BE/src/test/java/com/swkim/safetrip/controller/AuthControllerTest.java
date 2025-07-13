e7bdb2f25159d809bb4b9ed0b7082bb2055fd1c4
swkim0911 <qqazws7@konkuk.ac.kr>
2025-07-13 15:12:23 +0900

    [#44] test: 액세스 토큰 재발급 요청시 리프레시 토큰이 유효하지만 대응하는 유저를 찾을 수 없는 경우 리프레시 토큰 재사용으로 간주하고 401 에러가 발생하는 테스트 코드 작성

--- BE/src/test/java/com/swkim/safetrip/controller/AuthControllerTest.java
+++ BE/src/test/java/com/swkim/safetrip/controller/AuthControllerTest.java
@@ -8,6 +8,7 @@ import com.swkim.safetrip.dto.response.AccessTokenResponse;
 import com.swkim.safetrip.global.exception.custom.InvalidRefreshTokenException;
 import com.swkim.safetrip.global.exception.custom.RefreshTokenExpiredException;
 import com.swkim.safetrip.global.exception.custom.RefreshTokenMissingException;
+import com.swkim.safetrip.global.exception.custom.RefreshTokenReuseDetectedException;
 import com.swkim.safetrip.jwt.JwtUtils;
 import com.swkim.safetrip.service.AuthService;
 import com.swkim.safetrip.service.UserService;
@@ -25,6 +26,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -148,6 +150,23 @@ class AuthControllerTest {
                 .andExpect(result -> assertInstanceOf(InvalidRefreshTokenException.class, result.getResolvedException()))
                 .andExpect(jsonPath("$.code").value(401))
                 .andExpect(jsonPath("$.message").value("Refresh token is invalid"));
+    }
+
+    @Test
+    void 액세스_토큰_재발급_요청시_리프레시_토큰이_유효하지만_대응하는_유저를_찾을_수_없는_경우_리프레시_토큰_재사용으로_간주하고_401_에러가_발생한다() throws Exception {
+        // given
+        String reUsedRefreshToken = "reused.refresh.token";
+        doNothing().when(jwtUtils).validateRefreshToken(reUsedRefreshToken);
+        when(authService.reIssueAccessToken(reUsedRefreshToken))
+                .thenThrow(new RefreshTokenReuseDetectedException());
+
+        // when & then
+        mockMvc.perform(post("/auth/refresh")
+                        .cookie(new Cookie("refreshToken", reUsedRefreshToken)))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.code").value(401))
+                .andExpect(jsonPath("$.message").value("Refresh token reuse detected"));
+
     }
 
 
