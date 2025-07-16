bb8e074d8789b130cc3c2df21047fa5c887744de
swkim0911 <qqazws7@konkuk.ac.kr>
2025-07-16 15:01:52 +0900

    [#44] feat: 리프레시 토큰 저장 로직 구현

---
+++ BE/src/main/java/com/swkim/safetrip/service/TokenService.java
@@ -0,0 +1,18 @@
+package com.swkim.safetrip.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void saveRefreshToken(String email, String refreshToken, long expirationMillis) {
+        long expirationSeconds = expirationMillis / 1000;
+        redisTemplate.opsForValue().set("refresh:" + email, refreshToken, expirationSeconds, TimeUnit.SECONDS);
+    }
+}
