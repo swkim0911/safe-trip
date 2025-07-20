fb327bf503a29a55c5d5ceb8ba00c775ed1ef3a9
swkim0911 <qqazws7@konkuk.ac.kr>
2025-07-20 16:47:21 +0900

    [#46] feat: CustomUserDetails 클래스 생성

---
+++ BE/src/main/java/com/swkim/safetrip/security/CustomUserDetails.java
@@ -0,0 +1,31 @@
+package com.swkim.safetrip.security;
+
+import com.swkim.safetrip.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().getAuthority()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
