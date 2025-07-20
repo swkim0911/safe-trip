55c4a15820d9b4f6e24f0e114332038a62f27d42
swkim0911 <qqazws7@konkuk.ac.kr>
2025-07-20 15:44:40 +0900

    [#46] test: UserDetails 객체 생성할 때 role 이름이 ROLE_ prefix를 갖고 있다면 예외가 발생하는 테스트 코드 작성

---
+++ BE/src/test/java/com/swkim/safetrip/service/LoginServiceTest.java
@@ -0,0 +1,41 @@
+package com.swkim.safetrip.service;
+
+import com.swkim.safetrip.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ExtendWith(MockitoExtension.class)
+class LoginServiceTest {
+
+    @InjectMocks
+    private LoginService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("role 이름이 \"ROLE_\"으로 시작하면 예외가 발생한다")
+    void roles_with_ROLE_prefix_should_throw_exception() {
+        // given
+        String roleWithPrefix = "ROLE_USER";
+
+        // when & then
+        assertThatThrownBy(() ->
+                org.springframework.security.core.userdetails.User.builder()
+                        .username("test@gmail.com")
+                        .password("password")
+                        .roles(roleWithPrefix)
+                        .build()
+        )
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("cannot start with ROLE_");
+    }
+
+
+}
