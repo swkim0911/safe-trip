30c90b67adcee712421287d832af3fb8a9ed3301
swkim0911 <qqazws7@konkuk.ac.kr>
2025-07-22 14:51:41 +0900

    [#48] test: 회원가입 요청시 잘못된 비밀번호 입력시 예외가 발생하는 테스트 코드 작성

--- BE/src/test/java/com/swkim/safetrip/controller/UserControllerTest.java
+++ BE/src/test/java/com/swkim/safetrip/controller/UserControllerTest.java
@@ -13,9 +13,9 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,21 +42,49 @@ public class UserControllerTest {
         //given
         UserSignUpRequest signUpRequest = UserSignUpRequest.builder()
                 .email("swkim@gmail.com")
-                .password("password")
+                .password("pass!word1")
                 .nickname("nickname")
                 .build();
 
+        given(userService.signup(signUpRequest)).willReturn(1L);
+
         //when
-        when(userService.signup(any(UserSignUpRequest.class))).thenReturn(1L);
+        ResultActions resultActions = mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpRequest)));
 
         //then
-        mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(signUpRequest)))
+        resultActions
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.code").value(201))
                 .andExpect(jsonPath("$.result").value(1L));
     }
+
+    @Test
+    void 회원가입_요청시_비밀번호_검증에_통과하지_못하면_예외가_발생한다() throws Exception {
+        //given
+        UserSignUpRequest signUpRequest = UserSignUpRequest.builder()
+                .email("swkim@gmail.com")
+                .password("invalid123")
+                .nickname("nickname")
+                .build();
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpRequest)));
+
+        //then
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("Invalid request"));
+    }
+
+
+
+
+
     @Test
     void 존재하지_않는_URL_요청시_404응답이_반환된다() throws Exception {
 
