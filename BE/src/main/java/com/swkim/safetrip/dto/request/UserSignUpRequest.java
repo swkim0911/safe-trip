6d8347003c04218a686163673b874cd4c5e8d9bc
swkim0911 <qqazws7@konkuk.ac.kr>
2025-07-22 14:36:09 +0900

    [#48] feat: 회원가입시 비밀번호, 닉네임 validation 조건 추가

--- BE/src/main/java/com/swkim/safetrip/dto/request/UserSignUpRequest.java
+++ BE/src/main/java/com/swkim/safetrip/dto/request/UserSignUpRequest.java
@@ -2,6 +2,8 @@ package com.swkim.safetrip.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,12 +16,23 @@ import lombok.NoArgsConstructor;
 public class UserSignUpRequest {
 
     @Email
-    @NotBlank(message = "please enter your e-mail address")
+    @NotBlank(message = "please enter your e-mail address.")
     private String email;
 
-    @NotBlank(message = "please enter your password")
+
+    @Size(min = 8, max = 20, message = "password must be between 8 and 20 characters.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+=-]).*$",
+            message = "password must include letters, numbers, and special characters."
+    )
+    @NotBlank(message = "please enter your password.")
     private String password;
 
-    @NotBlank(message = "please enter nickname")
+    @Size(min = 2, max = 15, message = "nickname must be between 2 and 15 characters.")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9가-힣_-]+$",
+            message = "nickname can contain Korean, English letters, digits, _ and - only."
+    )
+    @NotBlank(message = "please enter nickname.")
     private String nickname;
 }
