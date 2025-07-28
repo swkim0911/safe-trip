b6178dece6305cfa5dd263011e05f048be3e3cf4
swkim0911 <qqazws7@konkuk.ac.kr>
2025-07-28 13:38:27 +0900

    [#43] refactor: 로그인 모달창 닫기 버튼을 Js로 동작하게 변경

--- FE/src/components/AuthFormModal.vue
+++ FE/src/components/AuthFormModal.vue
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-md">
       <div class="modal-content">
           <div class="modal-header border-bottom-0">
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button type="button" class="btn-close" @click="closeModal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
             <div v-if="mode === 'login'">
