ac58f7a9cc1d9081cbf7850aa2db7f5a47157364
swkim0911 <qqazws7@konkuk.ac.kr>
2025-07-29 13:57:48 +0900

    [#53] refactor: 함수 이름 변경

--- BE/src/main/java/com/swkim/safetrip/controller/UserController.java
+++ BE/src/main/java/com/swkim/safetrip/controller/UserController.java
@@ -42,7 +42,7 @@ public class UserController {
 
     @Operation(summary = "닉네임 사용 가능 여부 확인", description = "입력한 닉네임이 형식에 맞고, 중복 여부를 확인합니다.")
     @GetMapping("users/validate-nickname")
-    public ApiResult<ValidationResponse> checkNicknameDuplicate(@RequestParam String nickname) {
+    public ApiResult<ValidationResponse> validateNickname(@RequestParam String nickname) {
         return ApiResult.of(HttpStatus.OK.value(), "completed duplicate check of nickname", userService.validateNickname(nickname));
     }
 
