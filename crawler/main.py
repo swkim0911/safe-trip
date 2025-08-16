9c31465c84746c88288f6dc678919a499beb0120
swkim0911 <qqazws7@konkuk.ac.kr>
2025-08-16 17:14:47 +0900

    [#60] feat: prompt 생성 함수 구현

--- crawler/main.py
+++ crawler/main.py
@@ -1,6 +1,8 @@
 from reddit_client import get_instance
 import metadata_crawler
+from prompt import prompt_manager
+
 
 if __name__ == "__main__":
     reddit = get_instance()
-    metadata_crawler.crawl_subreddit_metadata(reddit)
+    result = prompt_manager.generate_classification_prompt("hello")
---
+++ crawler/prompt/prompt_manager.py
@@ -0,0 +1,17 @@
+def generate_classification_prompt(text):
+
+    with open("prompt/classification_prompt.txt", "r", encoding="utf-8") as f:
+        PROMPT_TEMPLATE = f.read()
+
+    prompt = PROMPT_TEMPLATE.format(text=text)
+
+    return prompt
+
+
+def generate_extraction_prompt(text):
+    with open("prompt/extraction_prompt.txt", "r", encoding="utf-8") as f:
+        PROMPT_TEMPLATE = f.read()
+
+    prompt = PROMPT_TEMPLATE.format(text=text)
+
+    return prompt
