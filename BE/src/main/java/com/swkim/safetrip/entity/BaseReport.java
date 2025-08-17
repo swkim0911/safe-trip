4089061812a611db05fb92b7a758f5d034ccdf97
swkim0911 <qqazws7@konkuk.ac.kr>
2025-08-17 16:03:33 +0900

    [#61] feat: BaseReport 엔티티 생성

---
+++ BE/src/main/java/com/swkim/safetrip/entity/BaseReport.java
@@ -0,0 +1,27 @@
+package com.swkim.safetrip.entity;
+
+import jakarta.persistence.*;
+import lombok.Setter;
+
+@MappedSuperclass
+public abstract class BaseReport extends BaseEntity {
+
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "scam_id")
+    private Scam scam;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false, length = 1000)
+    private String description;
+}
