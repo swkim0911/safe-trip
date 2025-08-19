14133e1bfdf5fa0a087e03537f398f166998fcdf
swkim0911 <qqazws7@konkuk.ac.kr>
2025-08-19 22:12:36 +0900

    [#61] feat: external report 상세 조회 비즈니스 로직 구현

--- BE/src/main/java/com/swkim/safetrip/service/ExternalReportService.java
+++ BE/src/main/java/com/swkim/safetrip/service/ExternalReportService.java
@@ -1,6 +1,7 @@
 package com.swkim.safetrip.service;
 
 import com.swkim.safetrip.dto.response.ExternalReportDetailResponse;
+import com.swkim.safetrip.global.exception.custom.ReportNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,8 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ExternalReportService {
 
+    private final ExternalReportRepository externalReportRepository;
+
     @Transactional(readOnly = true)
     public ExternalReportDetailResponse getExternalReport(Long id){
-
+        return externalReportRepository.findReportDetailById(id).orElseThrow(ReportNotFoundException::new);
     }
 }
