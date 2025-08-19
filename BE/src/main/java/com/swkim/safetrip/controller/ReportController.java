0a5c4cadc76707c30dce98c40ee96e18cc34ae75
swkim0911 <qqazws7@konkuk.ac.kr>
2025-08-19 18:11:39 +0900

    [#61] refactor: ReportController에서 url 공통부분 묶기

--- BE/src/main/java/com/swkim/safetrip/controller/ReportController.java
+++ BE/src/main/java/com/swkim/safetrip/controller/ReportController.java
@@ -12,12 +12,10 @@ import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/reports")
 @RequiredArgsConstructor
 public class ReportController {
 
@@ -25,28 +23,28 @@ public class ReportController {
     private final MessageSource messageSource;
 
     @Operation(summary = "국가별 스캠 요약 정보 조회", description = "사이드바에 표현될 국가별 스캠 요약 정보를 조회합니다")
-    @GetMapping(value = "/reports/sidebar-summary/counties")
+    @GetMapping(value = "/sidebar-summary/counties")
     public ApiResult<Slice<LocationScamSummaryItem>> getCountrySummariesForSideBar(Pageable pageable){
         Slice<LocationScamSummaryItem> countrySummaryPages = reportService.getCountrySummaryPages(pageable);
         return ApiResult.of(HttpStatus.OK.value(), "Country scam summaries for sidebar", countrySummaryPages);
     }
 
     @Operation(summary = "도시별 스캠 요약 정보 조회", description = "사이드바에 표현될 도시(state)별 스캠 요약 정보를 조회합니다")
-    @GetMapping(value = "/reports/sidebar-summary/states")
+    @GetMapping(value = "/sidebar-summary/states")
     public ApiResult<Slice<LocationScamSummaryItem>> getStateSummariesForSidebar(@RequestParam Long countryId, Pageable pageable){
         Slice<LocationScamSummaryItem> stateSummaryPage = reportService.getStateSummaryPages(countryId, pageable);
         return ApiResult.of(HttpStatus.OK.value(), "State scam summaries for sidebar", stateSummaryPage);
     }
 
     @Operation(summary = "스캠 요약 정보 조회", description = "사이드바에 표현될 스캠 요약 정보를 조회합니다")
-    @GetMapping(value = "/reports/sidebar-summary/reports")
+    @GetMapping(value = "/sidebar-summary/reports")
     public ApiResult<Slice<ReportSummaryItem>> getReportSummariesForSidebar(@RequestParam Long countryId, @RequestParam Long cityId, Pageable pageable) {
         Slice<ReportSummaryItem> reportSummaryPages = reportService.getReportSummaryPages(countryId, cityId, pageable);
         return ApiResult.of(HttpStatus.OK.value(), "Report summaries for sidebar", reportSummaryPages);
     }
 
     @Operation(summary = "특정 리포트 조회", description = "특정 리포트를 아이디로 조회합니다")
-    @GetMapping(value = "/reports/{reportId}")
+    @GetMapping(value = "/{reportId}")
     public ApiResult<ReportFindByIdResponse> getReport(@PathVariable Long reportId) {
 
         ReportFindByIdResponse report = reportService.getReport(reportId);
@@ -56,7 +54,7 @@ public class ReportController {
     }
 
     @Operation(summary = "지도에 표시될 스캠 정보 조회", description = "zoom 정도에 따라 도시에 표시될 스캠 요약 정보를 조회합니다")
-    @GetMapping(value = "/reports/map-summary")
+    @GetMapping(value = "/map-summary")
     public ApiResult<LocationScamSummaryResponse> getLocationSummariesForMap(@RequestParam Integer zoom){
         if(zoom < 7){
             LocationScamSummaryResponse countrySummaries = reportService.getCountrySummaries();
