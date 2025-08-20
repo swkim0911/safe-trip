b65710c0ba6297570d6d16fb340f829b84ef9533
swkim0911 <qqazws7@konkuk.ac.kr>
2025-08-20 10:08:22 +0900

    [#61] refactor: 사용하지 않는 함수 삭제

--- BE/src/main/java/com/swkim/safetrip/mapper/ReportMapper.java
+++ BE/src/main/java/com/swkim/safetrip/mapper/ReportMapper.java
@@ -1,11 +1,8 @@
 package com.swkim.safetrip.mapper;
 
 import com.swkim.safetrip.dto.request.ReportSaveRequest;
-import com.swkim.safetrip.dto.response.UserReportDetailResponse;
 import com.swkim.safetrip.entity.UserReport;
 import com.swkim.safetrip.entity.enums.Source;
-
-import java.util.List;
 
 public class ReportMapper {
 
@@ -16,19 +13,4 @@ public class ReportMapper {
                 .description(reportSaveRequest.getDescription())
                 .build();
     }
-
-    public static UserReportDetailResponse toReportFindByIdResponse(UserReport userReport, List<String> URLs) {
-        return UserReportDetailResponse.builder()
-                .title(userReport.getTitle())
-                .scam(userReport.getScam().getName())
-                .address(userReport.getLocation().getAddress())
-                .lat(String.valueOf(userReport.getLocation().getLat()))
-                .lng(String.valueOf(userReport.getLocation().getLng()))
-                .URLs(URLs)
-                .description(userReport.getDescription())
-                .advice(userReport.getAdvice())
-                .createdAt(userReport.getCreatedAt())
-                .build();
-    }
-
 }
