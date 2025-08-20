6fb08e1ccc86044caa4e29e753e7e059cc8d174a
swkim0911 <qqazws7@konkuk.ac.kr>
2025-08-20 18:34:47 +0900

    [#60] refactor: 패키지 구조 변경

--- crawler/src/main.py
+++ crawler/src/main.py
@@ -3,4 +3,4 @@ import travel_scam_crawler
 
 if __name__ == "__main__":
     reddit = get_instance()
-    travel_scam_crawler.crawl_travel_scam(reddit, 1)
+    # travel_scam_crawler.crawl_travel_scam(reddit, 1)
