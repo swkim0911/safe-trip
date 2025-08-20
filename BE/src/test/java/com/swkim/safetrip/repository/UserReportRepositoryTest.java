002badf3b9fa9b817771736963db82e10cf24e32
swkim0911 <qqazws7@konkuk.ac.kr>
2025-08-20 10:15:44 +0900

    [#61] test: DB 구조 변경으로 필요없는 테스트 삭제

--- BE/src/test/java/com/swkim/safetrip/repository/UserReportRepositoryTest.java
+++ BE/src/test/java/com/swkim/safetrip/repository/UserReportRepositoryTest.java
@@ -1,10 +1,6 @@
 package com.swkim.safetrip.repository;
 
 import com.swkim.safetrip.config.QuerydslConfig;
-import com.swkim.safetrip.entity.UserReport;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
@@ -12,30 +8,4 @@ import org.springframework.context.annotation.Import;
 @Import({QuerydslConfig.class})
 class UserReportRepositoryTest {
 
-    @Autowired
-    private ReportRepository reportRepository;
-
-    @Test
-    @DisplayName("findReportWithLocationById 함수 지연로딩 확인")
-    void lazy_loading_test() {
-        //given
-        Location location = Location.builder()
-                .address("downtown")
-                .lat("31.123")
-                .lng("102.13925")
-                .build();
-
-        UserReport userReport = UserReport.builder()
-                .title("title")
-                .description("description")
-                .build();
-
-        UserReport saveUserReport = reportRepository.save(userReport);
-
-        //when
-
-        //then
-//        boolean isProxy = mockingDetails(findUserReport.getLocation()).isMock(); // fetch join이면 location 객체는 proxy가 아니다.
-//        Assertions.assertThat(isProxy).isFalse();
-    }
 }
