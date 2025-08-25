cab0d0f66819ec9da6d296b0cc8eeb46fbb709dd
swkim0911 <qqazws7@konkuk.ac.kr>
2025-08-25 15:33:02 +0900

    [#64] refactor: response dto record class로 수정-2

--- BE/src/main/java/com/swkim/safetrip/dto/response/LocationScamSummaryResponse.java
+++ BE/src/main/java/com/swkim/safetrip/dto/response/LocationScamSummaryResponse.java
@@ -1,20 +1,14 @@
 package com.swkim.safetrip.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class LocationScamSummaryResponse {
-
-    private LocationType locationType; // country or state
-    private List<LocationScamSummaryItem> items;
+public record LocationScamSummaryResponse(
+        LocationType locationType,// country or state
+        List<LocationScamSummaryItem> items
+) {
 
     public enum LocationType {
         COUNTRY,
