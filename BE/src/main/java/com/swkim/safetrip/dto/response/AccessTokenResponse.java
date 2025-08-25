04f7c20858dd0a1b1ea0bbf3e062229ddb8634f5
swkim0911 <qqazws7@konkuk.ac.kr>
2025-08-25 15:27:30 +0900

    [#64] refactor: response dto record class로 수정-1

--- BE/src/main/java/com/swkim/safetrip/dto/response/AccessTokenResponse.java
+++ BE/src/main/java/com/swkim/safetrip/dto/response/AccessTokenResponse.java
@@ -1,15 +1,10 @@
 package com.swkim.safetrip.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class AccessTokenResponse {
+public record AccessTokenResponse(
+        String accessToken
+){
 
-    private String accessToken;
 }
