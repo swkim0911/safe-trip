5dd891122a6861f7aed4de942f221bd5014a439e
swkim0911 <qqazws7@konkuk.ac.kr>
2025-08-26 12:33:29 +0900

    [#60] chore: 원활한 테스트 진행을 위해 프로젝트 빌드 설정으로 src/ 안에 있는 모듈 등록

--- crawler/tests/test_crawler.py
+++ crawler/tests/test_crawler.py
@@ -1,9 +1,6 @@
-from src.adapters.reddit_client import get_instance
-from dotenv import load_dotenv
-from datetime import datetime
-import time
 import praw
-
+from crawler import travel_scam_classifier
+from crawler import travel_scam_extractor
 
 KEYWORDS = ['"travel scam"']
 
@@ -14,25 +11,24 @@ def clean_text(text: str) -> str:
     return text.replace("\u200b", "").strip()
 
 
-def crawl_travel_scam(reddit: praw.Reddit, limit: int) -> None:
+def crawl_travel_scam_with_url(reddit: praw.Reddit, limit: int, url: str) -> None:
 
-    subreddit = reddit.subreddit('travel')
+    submission = reddit.submission(url=url)  # url로 직접 게시글 가져오기
+    post_body = clean_text(submission.selftext)
 
-    for post in subreddit.search(' OR '.join(KEYWORDS), sort="relevance", time_filter="all", limit=limit):
-        post_body = clean_text(post.selftext)
+    is_travel_scam = travel_scam_classifier.classify(post_body)
+    # if is_travel_scam:
+    #     travel_scam_data = travel_scam_extractor.extract(post_body)
+    #     # todo: db에 저장
+    #     print(travel_scam_data)
 
-        is_travel_scam = travel_scam_classifier.classify(post_body)
-        if is_travel_scam:
-            travel_scam_data = travel_scam_extractor.extract(post_body)
-            # todo: db에 저장
+    # post.comments.replace_more(limit=0)
 
-        post.comments.replace_more(limit=0)
+    # # depth=1 댓글만 선택 (top-level comments)
+    # for i, comment in enumerate(post.comments):
 
-        # depth=1 댓글만 선택 (top-level comments)
-        for i, comment in enumerate(post.comments):
-
-            comment_body = clean_text(comment.body)
-            is_travel_scam_cm = travel_scam_classifier.classify(comment_body)
-            if is_travl_scam_cm:
-                travel_scam_data = travel_scam_extractor.extract(commnet_body)
-                # todo: db에 저장
+    #     comment_body = clean_text(comment.body)
+    #     is_travel_scam_cm = travel_scam_classifier.classify(comment_body)
+    #     if is_travl_scam_cm:
+    #         travel_scam_data = travel_scam_extractor.extract(commnet_body)
+    #         # todo: db에 저장
