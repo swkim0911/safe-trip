302ee31c1b4ad3881d4a082c25d1b53d6d2bbd61
swkim0911 <qqazws7@konkuk.ac.kr>
2025-08-26 13:55:17 +0900

    [#60] test: classify 프롬프트 테스트 코드 작성

--- crawler/tests/test_collector.py
+++ crawler/tests/test_collector.py
@@ -1,5 +1,6 @@
 from collector import travel_scam_extractor
 from collector import travel_scam_classifier
+from adapters.reddit_client import get_instance
 import praw
 
 
@@ -12,24 +13,16 @@ def clean_text(text: str) -> str:
     return text.replace("\u200b", "").strip()
 
 
-def crawl_travel_scam_with_url(reddit: praw.Reddit, limit: int, url: str) -> None:
+def crawl_travel_scam_with_url(reddit: praw.Reddit, url: str) -> None:
 
     submission = reddit.submission(url=url)  # url로 직접 게시글 가져오기
     post_body = clean_text(submission.selftext)
 
     is_travel_scam = travel_scam_classifier.classify(post_body)
-    # if is_travel_scam:
-    #     travel_scam_data = travel_scam_extractor.extract(post_body)
-    #     # todo: db에 저장
-    #     print(travel_scam_data)
+    print(is_travel_scam)
 
-    # post.comments.replace_more(limit=0)
 
-    # # depth=1 댓글만 선택 (top-level comments)
-    # for i, comment in enumerate(post.comments):
-
-    #     comment_body = clean_text(comment.body)
-    #     is_travel_scam_cm = travel_scam_classifier.classify(comment_body)
-    #     if is_travl_scam_cm:
-    #         travel_scam_data = travel_scam_extractor.extract(commnet_body)
-    #         # todo: db에 저장
+if __name__ == "__main__":
+    reddit = get_instance()
+    url = 'https://www.reddit.com/r/travel/comments/13y9dqk/weird_new_travel_scam/'
+    crawl_travel_scam_with_url(reddit, url)
