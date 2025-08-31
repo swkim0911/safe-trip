85920f5e0af379c07070cd8cf0dafab5842f4586
swkim0911 <qqazws7@konkuk.ac.kr>
2025-08-31 13:54:11 +0900

    [#60] feat: mongo client 구현

---
+++ crawler/src/adapters/mongo_client.py
@@ -0,0 +1,17 @@
+
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+
+MONGO_DB = os.getenv("MONGO_DB", "safe_trip")
+
+client = MongoClient(MONGO_URI)
+db = client[MONGO_DB]
+
+
+def get_raw_collection():
+    return db["raw_reddit"]
