d888e5a3140e838d2c2572ee79c0d84d8ecc2759
swkim0911 <qqazws7@konkuk.ac.kr>
2025-09-03 15:31:01 +0900

    [#60] feat: world DB 조회 client 코드 작성

--- crawler/src/adapters/mongo_client.py
+++ crawler/src/adapters/mongo_client.py
@@ -7,14 +7,25 @@ load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
 
-MONGO_DB = os.getenv("MONGO_DB", "safe_trip")
+SAFETRIP_DB = os.getenv("SAFETRIP_DB", "safe_trip")
+WORLD_DB = os.getenv("WORLD_DB", "world")
 
 client = MongoClient(MONGO_URI)
-db = client[MONGO_DB]
+safetrip_db = client[SAFETRIP_DB]
+world_db = client[WORLD_DB]
 
 
 def get_raw_collection():
-    return db["raw_reddit"]
+    return safetrip_db["raw_reddit"]
 
 def get_parsed_collection():
-    return db["parsed_reddt"]
+    return safetrip_db["parsed_reddt"]
+
+def get_country_collection():
+    return world_db["countries"]
+
+def get_state_collection():
+    return world_db["states"]
+
+def get_city_collection():
+    return world_db["cities"]
