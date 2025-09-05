1eb5fdf13ea2142ef02a138fcb0caac8b99a434a
swkim0911 <qqazws7@konkuk.ac.kr>
2025-09-06 07:13:32 +0900

    [#60] refactor: extractor 클래스화

--- crawler/src/collector/extractor/travel_scam_extractor.py
+++ crawler/src/collector/extractor/travel_scam_extractor.py
@@ -3,89 +3,64 @@ from pymongo import UpdateOne
 from datetime import datetime, UTC
 import praw
 
-KEYWORDS = ["travel scam"]
-BATCH_SIZE = 100
+class TravelScamExtractor:
+    def __init__(self, reddit: praw.Reddit, redditRepository):
+        self.reddit = reddit
+        self.redditRepository = redditRepository 
+        self.BATCH_SIZE = 100
+        self.operations = []
+        self.keywords = ["travel scam"]
 
-'''
-    reddit에 있는 travel scam raw data -> mongoDB에 json 형태로 저장
-
-    @Args: 
-        time_filter: hour/day/week/month/year/all 중 하나
-        limit: 가져올 최대 게시글 수
-'''
-def extract(reddit: praw.Reddit, time_filter: str, limit: int):
-    raw_collection = get_raw_collection()
-    subreddit = reddit.subreddit("travel")
-    operations = []
-
-    def flush_ops(ops):
-        # operations를 bulk_write 실행 후 비움
-        if ops:
-            result = raw_collection.bulk_write(ops, ordered=False)
-            print(f"Matched: {result.matched_count}, "
-                  f"Modified: {result.modified_count}, "
-                  f"Upserted: {len(result.upserted_ids)}")
-            ops.clear()
-
-    for post in subreddit.search(' OR '.join(KEYWORDS), sort="relevance", time_filter=time_filter, limit=limit):
-
-        post_doc = {
-            "reddit_id": f"t3_{post.id}",
-            "source": "reddit",
-            "author": str(post.author) if post.author else None,
-            "body": post.selftext,
-            "url": f"https://reddit.com{post.permalink}",
-            "type": "post",
-            "posted_at": datetime.utcfromtimestamp(post.created_utc)
-        }
+    def __add_operation(self, reddit_id: str, doc: dict):
         now = datetime.now(UTC)
-        operations.append(
+        self.operations.append(
             UpdateOne(
-                {"reddit_id": f"t3_{post.id}"},
+                {"reddit_id": reddit_id},
                 {
-                    "$set": {
-                        **post_doc,
-                        "modified_at": now  # 매번 업데이트됨
-                    },
-                    "$setOnInsert": {
-                        "created_at": now  # 새로 insert될 때만 설정
-                    }
+                    "$set": {**doc, "modified_at": now},
+                    "$setOnInsert": {"created_at": now}
                 },
                 upsert=True
             )
         )
+        if len(self.operations) >= self.BATCH_SIZE:
+            self.redditRepository.flush_raw_ops(self.operations)
+    
+    '''
+    reddit에 있는 travel scam raw data -> mongoDB에 json 형태로 저장
 
-        post.comments.replace_more(limit=0)
-        for comment in post.comments:
-            if comment.parent_id.startswith("t3_"):
-                comment_doc = {
-                    "reddit_id": f"t1_{comment.id}",
-                    "source": "reddit",
-                    "author": str(comment.author) if comment.author else None,
-                    "body": comment.body,
-                    "url": f"https://reddit.com{comment.permalink}",
-                    "type": "comment",
-                    "posted_at": datetime.utcfromtimestamp(comment.created_utc),
-                }
-                now = datetime.now(UTC)
-                operations.append(
-                    UpdateOne(
-                        {"reddit_id": f"t1_{comment.id}"},
-                        {
-                            "$set": {
-                                **comment_doc,
-                                "modified_at": now  # 매번 업데이트됨
-                            },
-                            "$setOnInsert": {
-                                "created_at": now  # 새로 insert될 때만 설정
-                            }
-                        },
-                        upsert=True
-                    )
-                )
+    @Args:
+        time_filter: hour/day/week/month/year/all 중 하나
+        limit: 가져올 최대 게시글 수
+    '''
+    def extract(self, time_filter: str, limit: int):
+        subreddit = self.reddit.subreddit("travel")
 
-            if len(operations) >= BATCH_SIZE:
-                flush_ops(operations)
+        for post in subreddit.search(" OR ".join(self.keywords), sort="relevance", time_filter=time_filter, limit=limit):
+            post_doc = {
+                "reddit_id": f"t3_{post.id}",
+                "source": "reddit",
+                "author": str(post.author) if post.author else None,
+                "body": post.selftext,
+                "url": f"https://reddit.com{post.permalink}",
+                "type": "post",
+                "posted_at": datetime.utcfromtimestamp(post.created_utc)
+            }
+            self.__add_operation(post_doc["reddit_id"], post_doc)
 
-    # 남은 operations 처리
-    flush_ops(operations)
+            post.comments.replace_more(limit=0)
+            for comment in post.comments:
+                if comment.parent_id.startswith("t3_"):
+                    comment_doc = {
+                        "reddit_id": f"t1_{comment.id}",
+                        "source": "reddit",
+                        "author": str(comment.author) if comment.author else None,
+                        "body": comment.body,
+                        "url": f"https://reddit.com{comment.permalink}",
+                        "type": "comment",
+                        "posted_at": datetime.utcfromtimestamp(comment.created_utc),
+                    }
+                    self.__add_operation(comment_doc["reddit_id"], comment_doc)
+        
+        # 마지막 flush
+        self.redditRepository.flush_raw_ops(self.operations)
