c692a46572cf80fa8846e7eafad692222e60124a
swkim0911 <qqazws7@konkuk.ac.kr>
2025-09-05 10:33:58 +0900

    [#60] test: db lookup 기능 tdd로 구현

--- crawler/tests/test_transformer.py
+++ crawler/tests/test_transformer.py
@@ -2,11 +2,10 @@ from collector.transformer import travel_scam_classifier
 from collector.transformer import travel_scam_parser
 from datetime import datetime, timedelta, UTC
 
-from adapters.mongo_client import get_parsed_collection, get_raw_collection
+from adapters import mongo_client
 from pymongo import InsertOne
 
 from adapters.reddit_client import get_instance
-
 import time
 
 BATCH_SIZE = 100
@@ -17,40 +16,95 @@ def clean_text(text: str) -> str:
         return ""
     return text.replace("\u200b", "").strip()
 
-
-'''
-    MongoDB에서 Reddit raw 데이터를 가져와서
-    travel scam으로 분류/추출한 뒤 정제된 결과를 MongoDB에 저장한다.
-
-    Args:
-        time_filter (str): "all" 또는 "7d" (최근 7일)
-'''
-
-
 def transform(time_filter: str):
-    raw_collection = get_raw_collection()
-    parsed_collection = get_parsed_collection()
+    raw_collection = mongo_client.get_raw_collection()
+    parsed_collection = mongo_client.get_parsed_collection()
+    country_collection = mongo_client.get_country_collection()
+    state_collection = mongo_client.get_state_collection()
+    city_collection = mongo_client.get_city_collection()
+
     operations = []
-    cnt = 0
 
     def flush_ops(ops):
         # operations를 bulk_write 실행 후 비움
         if ops:
-            result = parsed_collection.bulk_write(ops, ordered=False)
-            print(f"Inserted {len(ops)} docs into parsed_collection")
-            ops.clear()
+            try:
+                result = parsed_collection.bulk_write(ops, ordered=False)
+                print(f"Inserted {len(ops)} docs into parsed_collection")
+            except Exception as e:
+                print(f"[ERROR] Bulk write failed: {e}")
+            finally:
+                ops.clear()
 
+    def lookup_location(country_name, state_name, city_name):
+    
+        quries = {"country:state:city": {"name": city_name, "state_name": state_name, "country_name": country_name},
+                  "country:city": {"name": city_name, "country_name": country_name},
+                  "state:city": {"name": city_name, "state_name": state_name},
+                  "country:state": {"name": state_name, "country_name": country_name},
+                  "city": {"name": city_name}, 
+                  "state": {"name": state_name}, 
+                  "country": {"name": country_name}}
+
+        def find_city(query):
+          doc = city_collection.find_one(query, {"_id": 1, "country_id": 1, "state_id": 1}, collation={"locale": "en", "strength": 1})
+          if not doc:
+              return None
+            
+          doc["city_id"] = doc.pop("_id")
+          return doc
+
+        def find_state(query):
+            doc = state_collection.find_one(query, {"_id": 1, "country_id": 1}, collation={"locale": "en", "strength": 1})
+            if not doc:
+              return None
+            
+            doc["state_id"] = doc.pop("_id")
+            return doc
+
+        def find_country(query):
+            doc = country_collection.find_one(query, {"_id": 1}, collation={ "locale": "en", "strength": 1})
+            if not doc:
+              return None
+
+            doc["country_id"] = doc.pop("_id")
+            return doc
+          
+        match (bool(country_name), bool(state_name), bool(city_name)):
+          case (True, True, True):
+              return find_city(quries["country:state:city"]) or find_city(quries["country:city"]) or find_city(quries["state:city"]) or find_state(quries["country:state"]) or find_city(quries["city"]) or find_state(quries["state"]) or find_country(quries["country"])
+          case (True, True, False):
+              return find_state(quries["country:state"]) or find_state(quries["state"]) or find_country(quries["country"])
+          case (True, False, True):
+              return find_city(quries["country:city"]) or find_city(quries["city"]) or find_country(quries["country"])
+          case (True, False, False):
+              return find_country(quries["country"])
+          case (False, True, True):
+              return find_city(quries["state:city"]) or find_city(quries["city"]) or find_state(quries["state"])
+          case (False, True, False):
+  
+              return find_state(quries["state"])
+              # 여러 개여도 저장
+          case (False, False, True):
+              return find_city(quries["city"])
+          case _:
+              return None
+            
     # time_filter 조건 설정
     query = {}
+    existing_ids = set()
+
     if time_filter == "7d":
         one_week_ago = datetime.utcnow() - timedelta(days=7)
         query = {"posted_at": {"$gte": one_week_ago}}
+        existing_ids = set(parsed_collection.distinct(
+            "reddit_id",
+            {"posted_at": {"$gte": one_week_ago}}
+        ))
 
     # MongoDB에서 데이터 가져오기
     raw_jsons = raw_collection.find(query).limit(20)
-
-    existing_ids = set(parsed_collection.distinct("reddit_id"))
-
+    cnt = 0
     for raw_json in raw_jsons:
         # raw_json의 id가 이미 parsed 컬랙션에 있으면 continue (llm api 비용 감면을 위해)
         if raw_json.get("reddit_id") in existing_ids:
@@ -64,11 +118,18 @@ def transform(time_filter: str):
         is_scam = travel_scam_classifier.classify(body)
         if not is_scam:
             continue
-        cnt += 1
+
         parsed_scams = travel_scam_parser.parse(body) or []
 
         for scam_record in parsed_scams:
+          
 
+
+            # db look up
+            location_info = lookup_location(scam_record.get("country"), scam_record.get("state"), scam_record.get("city"))
+            if location_info is None:
+                continue
+            
             now = datetime.now(UTC)
 
             doc = {
@@ -79,36 +140,35 @@ def transform(time_filter: str):
                 "title": scam_record.get("title"),
                 "action": scam_record.get("action"),
                 "context": scam_record.get("context"),
-                "country": scam_record.get("country"),
-                "state": scam_record.get("state"),
-                "city": scam_record.get("city"),
+                "country_id": location_info.get("country_id"),
+                "state_id": location_info.get("state_id"),
+                "city_id": location_info.get("city_id"),
                 "summary": scam_record.get("summary"),
                 "posted_at": raw_json.get("posted_at"),
                 "created_at": now,
                 "modified_at": now
             }
+            cnt += 1
+            print(doc)
+            if cnt > 0: return
 
             # data insert (단순 insert)
             # operations.append(InsertOne(doc))
-            
-            print(doc)
 
-            # batch 저장
+            # # batch 저장
             # if len(operations) >= BATCH_SIZE:
             #     flush_ops(operations)
 
     # 남은 operations 처리
     # flush_ops(operations)
     print(cnt)
-    
 
 if __name__ == "__main__":
     start = time.time()
 
-    reddit = get_instance()
-    
     transform("all")
-    
+
+
     end = time.time()
-    
+
     print(f"테스트 실행 시간: {end - start:.2f} 초")
