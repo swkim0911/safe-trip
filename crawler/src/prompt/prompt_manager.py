93c7dc7c2cb67c53ddc8ae8614d0f260c0362e5c
swkim0911 <qqazws7@konkuk.ac.kr>
2025-09-11 10:36:58 +0900

    [#71] feat: system_content 파일로 부터 읽어오는 함수 구현

--- crawler/src/prompt/prompt_manager.py
+++ crawler/src/prompt/prompt_manager.py
@@ -7,6 +7,12 @@ def generate_classification_prompt(text):
 
     return prompt
 
+def get_classification_system_content():
+    
+    with open("data/prompt/classification_system_content.txt", "r", encoding="utf-8") as f:
+        system_content = f.read()
+        
+    return system_content
 
 def generate_parsing_prompt(text):
     with open("data/prompt/parsing_prompt.txt", "r", encoding="utf-8") as f:
@@ -15,3 +21,10 @@ def generate_parsing_prompt(text):
     prompt = PROMPT_TEMPLATE.format(text=text)
 
     return prompt
+
+def get_parsing_system_content():
+    
+    with open("data/prompt/parsing_system_content.txt", "r", encoding="utf-8") as f:
+        system_content = f.read()
+        
+    return system_content
