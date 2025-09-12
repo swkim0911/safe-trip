c46d5d6c37b53e4b2e9eb4e8991ed31d1a9b514a
swkim0911 <qqazws7@konkuk.ac.kr>
2025-09-12 14:51:06 +0900

    [#71] feat: 완료된 openai batch 요청의 결과를 가져오는 함수 구현

--- crawler/src/adapters/openai_client.py
+++ crawler/src/adapters/openai_client.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 from dotenv import load_dotenv
 from openai import OpenAI
 from functools import lru_cache
@@ -24,3 +24,47 @@ def call_openai_api(system_content:str, prompt: str, temperature: float = 0.0) -
         temperature=temperature,
     )
     return (response.output_text or "").strip()
+
+def call_openai_api_with_batch(filename: str):
+    client = get_openai_client()
+    
+    # 1. 파일 업로드
+    input_file = client.files.create(
+        file=open(filename, "rb"),
+        purpose="batch"
+    )
+    input_file_id = input_file.id
+
+    # 2. 배치 생성
+    response = client.batches.create(
+        input_file_id=input_file_id,
+        endpoint="/v1/responses",
+        completion_window="24h",
+    )
+    batch_id = response.id
+
+    return {"input_file_id": input_file_id, "batch_id": batch_id}
+
+def get_completed_batch_result(batch_id: str):
+    """
+        주어진 batch_id로 OpenAI Batch API 결과(JSONL)를 가져온다.
+        완료되지 않았으면 None 반환.
+    """
+    client = get_openai_client()
+
+    batch = client.batches.retrieve(batch_id)
+    if batch.status == "completed":
+        output_file_id = batch.output_file_id
+        file_obj = client.files.content(output_file_id)
+
+        # JSONL 콘텐츠 반환
+        content = file_obj.content.decode("utf-8")
+        return content
+
+    return None
+    # elif batch.status in ["failed", "expired", "cancelled"]:
+    #     logging.error(f"❌ Batch {batch_id} ended with status {batch.status}")
+    #     self.reddit_repository.mark_batch_as_failed(batch_id, batch.status)
+    # else:
+    #     logging.info(f"⏳ Batch {batch_id} still not ready, status={batch.status}")
+    #     return None
