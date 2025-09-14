ae7516730998b0549289add897ca867dec4dc026
swkim0911 <qqazws7@konkuk.ac.kr>
2025-09-14 13:44:59 +0900

    [#71] test: metadata 조회 -> 비용 계산 테스트 코드 변경

---
+++ crawler/tests/test_token_cost_expectation_all_text.py
@@ -0,0 +1,37 @@
+from repository.reddit_repository import RedditRepository
+from adapters import mongo_client
+import os, tiktoken
+
+from utils.prompt_utils import get_classification_system_content, generate_classification_prompt
+
+MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
+ENC = tiktoken.encoding_for_model(MODEL)
+
+def get_tokens_of_metadata():
+
+    reddit_repository = RedditRepository(mongo_client.get_raw_collection(), mongo_client.get_parsed_collection(),
+                                         mongo_client.get_batch_job_collection())
+
+    documents = reddit_repository.find_raw_documents({})
+
+    input_total_tokens = 0
+    output_total_tokens = 0
+
+    for document in documents:
+        body = document['body']
+        system_content = get_classification_system_content()
+        prompt = generate_classification_prompt(body)
+        input_total_tokens += len(ENC.encode(system_content)) + len(ENC.encode(prompt))
+        output_total_tokens += len(ENC.encode("1")) # 1 or 0 응답
+
+
+    input_cost = input_total_tokens / 1_000_000 * 0.15
+    output_cost = output_total_tokens / 1_000_000 * 0.60
+    total_cost = input_cost + output_cost
+
+    print(f"input tokens: {input_total_tokens}, cost: ${input_cost:.4f}")
+    print(f"output tokens: {output_total_tokens}, cost: ${output_cost:.4f}")
+    print(f"total cost: ${total_cost:.4f}")
+
+if __name__ == "__main__":
+    get_tokens_of_metadata()
