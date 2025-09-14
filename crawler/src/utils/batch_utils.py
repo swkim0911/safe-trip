fc363d15627a7cd0d8c4203d20b79e484e3ecd03
swkim0911 <qqazws7@konkuk.ac.kr>
2025-09-14 16:41:10 +0900

    [#71] refactor: write_jsonl 함수 리팩토링

--- crawler/src/utils/batch_utils.py
+++ crawler/src/utils/batch_utils.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from pathlib import Path
 import json, os
 
+MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
 BASE_DIR = Path(__file__).resolve().parent.parent.parent # /crawler
 
 def get_batch_filename(folder: str = "data/batch_inputs") -> str:
@@ -16,21 +17,30 @@ docs 배열을 받아 JSONL 파일로 변환한다.
 jsons: [{"reddit_id": "xxx", "body": "..."}, ...]
 return: 생성된 파일 경로
 """
-def write_jsonl(jsons: list[dict[str, str]], filename: str | None = None) -> str:
-    
-    system_content = prompt_utils.get_classification_system_content()
-    
-    if filename is None:
-            filename = get_batch_filename()
+def write_jsonl(jsons: list[dict[str, str]], job_type: str) -> str:
+
+    system_content_map = {
+        "classification": prompt_utils.get_classification_system_content,
+        "parsing": prompt_utils.get_parsing_system_content,
+    }
+    prompt_map = {
+        "classification": prompt_utils.generate_classification_prompt,
+        "parsing": prompt_utils.generate_parsing_prompt,
+    }
+
+    if job_type not in system_content_map:
+        raise ValueError(f"Unsupported job_type: {job_type}")
+
+    system_content = system_content_map[job_type]()
+    filename = get_batch_filename()
     
     with open(filename, "w", encoding="utf-8") as f:
-        MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
     
         for item in jsons:
             reddit_id = item["reddit_id"]
             body = item["body"]
             
-            prompt = prompt_utils.generate_classification_prompt(body)
+            prompt = prompt_map[job_type](body)
 
             req = {
                 "custom_id": reddit_id, 
