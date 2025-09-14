bf795795d3369004adac93c9069b44e7f55de402
swkim0911 <qqazws7@konkuk.ac.kr>
2025-09-14 14:37:03 +0900

    [#71] feat: parsing 요청 토큰 개수 측정 함수 구현

--- crawler/src/utils/token_utils.py
+++ crawler/src/utils/token_utils.py
@@ -1,4 +1,4 @@
-from utils.prompt_utils import get_classification_system_content, generate_classification_prompt
+from utils.prompt_utils import get_classification_system_content, generate_classification_prompt, get_parsing_system_content, generate_parsing_prompt
 import tiktoken, os
 
 MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
@@ -17,3 +17,15 @@ def estimate_classification_request_tokens(body):
             overhead
     )
 
+def estimate_parsing_request_tokens(body):
+    system_content = get_parsing_system_content()
+    prompt = generate_parsing_prompt(body)
+
+    # 메시지 2개(system + user) 오버헤드 고려
+    overhead = 8
+
+    return (
+            len(ENC.encode(system_content)) +
+            len(ENC.encode(prompt)) +
+            overhead
+    )
