4d8fbf9067d5e72151e55ad7abc4512c171f8ea2
swkim0911 <qqazws7@konkuk.ac.kr>
2025-09-19 14:59:06 +0900

    [#81] feat: 특정 country에 대한 states조회 엔드포인트 구현

--- BE/src/main/java/com/swkim/safetrip/controller/WorldController.java
+++ BE/src/main/java/com/swkim/safetrip/controller/WorldController.java
@@ -1,6 +1,7 @@
 package com.swkim.safetrip.controller;
 
 import com.swkim.safetrip.dto.response.world.CountriesResponse;
+import com.swkim.safetrip.dto.response.world.StatesResponse;
 import com.swkim.safetrip.global.response.ApiResult;
 import com.swkim.safetrip.service.CityService;
 import com.swkim.safetrip.service.CountryService;
@@ -8,6 +9,8 @@ import com.swkim.safetrip.service.StateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +25,12 @@ public class WorldController {
     public ApiResult<CountriesResponse> getAllCountries() {
         CountriesResponse allCountries = countryService.getAllCountries();
         return ApiResult.of(HttpStatus.OK.value(), "All countries retrieved successfully", allCountries);
+    }
+
+    @GetMapping("/countries/{countryId}/states")
+    public ApiResult<StatesResponse> getStates(@PathVariable Long countryId) {
+        StatesResponse allStates = stateService.getStates(countryId);
+        return ApiResult.of(HttpStatus.OK.value(), "All states retrieved successfully", allStates);
     }
 
 }
--- BE/src/main/java/com/swkim/safetrip/service/StateService.java
+++ BE/src/main/java/com/swkim/safetrip/service/StateService.java
@@ -1,10 +1,14 @@
 package com.swkim.safetrip.service;
 
+import com.swkim.safetrip.dto.response.world.StatesResponse;
+import com.swkim.safetrip.dto.response.world.StatesResponse.StateDto;
 import com.swkim.safetrip.entity.world.State;
 import com.swkim.safetrip.global.exception.custom.StateNotFoundException;
 import com.swkim.safetrip.repository.StateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,4 +20,11 @@ public class StateService{
         return stateRepository.findByIdWithCountry(id).orElseThrow(StateNotFoundException::new);
     }
 
+    public StatesResponse getStates(Long countryId) {
+        List<StateDto> states = stateRepository.findByCountryId(countryId).stream()
+                .map(s -> new StateDto(s.getId(), s.getName()))
+                .toList();
+
+        return new StatesResponse(states);
+    }
 }
