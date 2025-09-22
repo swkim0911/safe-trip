d5121165664409a855b7ace24893a39068b7e9d7
swkim0911 <qqazws7@konkuk.ac.kr>
2025-09-22 11:12:05 +0900

    [#97] fix: 크롤러 로깅 설정 변경

--- crawler/src/config/logging_config.py
+++ crawler/src/config/logging_config.py
@@ -1,19 +1,8 @@
 import logging
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent  # src/ 상위 디렉토리 (프로젝트 루트)
-LOG_DIR = BASE_DIR / "logs"
 
 def setup_logging(name: str, level=logging.INFO):
-    Path(LOG_DIR).mkdir(exist_ok=True)
-    logfile = Path(LOG_DIR) / f"{name}.log"
-
     logging.basicConfig(
         level=level,
-        format="%(asctime)s [%(levelname)s] %(name)s - %(message)s",
-        handlers=[
-            logging.FileHandler(logfile, encoding="utf-8"),
-            logging.StreamHandler()
-        ]
+        format="%(asctime)s [%(levelname)s] %(name)s - %(message)s"
     )
     return logging.getLogger(name)
