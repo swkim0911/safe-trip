24822c65e6447b10ce3c23f36ecbec7fbe465ecd
swkim0911 <qqazws7@konkuk.ac.kr>
2025-11-14 15:48:14 +0900

    [124] refactor: swagger api 문서 설명

--- BE/src/main/java/com/swkim/safetrip/controller/WorldController.java
+++ BE/src/main/java/com/swkim/safetrip/controller/WorldController.java
@@ -7,6 +7,7 @@ import com.swkim.safetrip.global.response.ApiResult;
 import com.swkim.safetrip.service.CityService;
 import com.swkim.safetrip.service.CountryService;
 import com.swkim.safetrip.service.StateService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,18 +24,30 @@ public class WorldController {
     private final StateService stateService;
     private final CityService cityService;
 
+    @Operation(
+            summary = "전체 국가 목록 조회",
+            description = "등록된 모든 국가 목록을 조회합니다."
+    )
     @GetMapping(value = "/countries")
     public ApiResult<CountriesResponse> getAllCountries() {
         CountriesResponse allCountries = countryService.getAllCountries();
         return ApiResult.of(HttpStatus.OK.value(), "All countries retrieved successfully", allCountries);
     }
 
+    @Operation(
+            summary = "특정 국가의 주(State) 목록 조회",
+            description = "선택한 국가에 속한 모든 주(State) 목록을 조회합니다."
+    )
     @GetMapping("/countries/{countryId}/states")
     public ApiResult<StatesResponse> getStates(@PathVariable Long countryId) {
         StatesResponse allStates = stateService.getStates(countryId);
         return ApiResult.of(HttpStatus.OK.value(), "All states retrieved successfully", allStates);
     }
 
+    @Operation(
+            summary = "특정 주(State)의 도시(City) 목록 조회",
+            description = "선택한 주(State)에 속한 모든 도시(City) 목록을 조회합니다."
+    )
     @GetMapping("/states/{stateId}/cities")
     public ApiResult<CitiesResponse> getCities(@PathVariable Long stateId) {
         CitiesResponse allCities = cityService.getCities(stateId);
