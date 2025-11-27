2246cdc041de7626fb99dd2dc617c00ed71c1273
swkim0911 <qqazws7@konkuk.ac.kr>
2025-11-27 14:40:28 +0900

    [#133] refactor: 프롬프트 경로 변경

--- crawler/src/utils/prompt_utils.py
+++ crawler/src/utils/prompt_utils.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 def generate_classification_prompt(text):
-    filename = BASE_DIR / "data" / "prompt" / "classification_prompt.txt"
+    filename = BASE_DIR / "data" / "prompt" / "classification" / "active" / "prompt.txt" 
     with open(filename, "r", encoding="utf-8") as f:
         PROMPT_TEMPLATE = f.read()
 
@@ -12,14 +12,14 @@ def generate_classification_prompt(text):
     return prompt
 
 def get_classification_system_content():
-    filename = BASE_DIR / "data" / "prompt" / "classification_system_content.txt"
+    filename = BASE_DIR / "data" / "prompt" / "classification" / "active" / "system_content.txt"
     with open(filename, "r", encoding="utf-8") as f:
         system_content = f.read()
         
     return system_content
 
 def generate_parsing_prompt(text):
-    filename = BASE_DIR / "data" / "prompt" / "parsing_prompt.txt"
+    filename = BASE_DIR / "data" / "prompt" / "parsing" / "active" / "prompt.txt"
     with open(filename, "r", encoding="utf-8") as f:
         PROMPT_TEMPLATE = f.read()
 
@@ -28,7 +28,7 @@ def generate_parsing_prompt(text):
     return prompt
 
 def get_parsing_system_content():
-    filename = BASE_DIR / "data" / "prompt" / "parsing_system_content.txt"
+    filename = BASE_DIR / "data" / "prompt" / "parsing" / "active" / "system_content.txt"
     with open(filename, "r", encoding="utf-8") as f:
         system_content = f.read()
         
