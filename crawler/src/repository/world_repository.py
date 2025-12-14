b036a655e00c2121ea36e93ea702cfbbc74315fa
swkim0911 <qqazws7@konkuk.ac.kr>
2025-12-14 15:28:20 +0900

    [#133] refactor: protected -> public 함수로 변경

--- crawler/src/repository/world_repository.py
+++ crawler/src/repository/world_repository.py
@@ -4,7 +4,7 @@ class WorldRepository:
         self.state_collection = state_collection
         self.city_collection = city_collection
 
-    def _find_city(self, query):
+    def find_city(self, query):
         doc = self.city_collection.find_one(
             query,
             {"_id": 1, "country_id": 1, "state_id": 1},
@@ -16,7 +16,7 @@ class WorldRepository:
         doc["city_id"] = doc.pop("_id")
         return doc
 
-    def _find_state(self, query):
+    def find_state(self, query):
         doc = self.state_collection.find_one(
             query,
             {"_id": 1, "country_id": 1},
@@ -28,18 +28,7 @@ class WorldRepository:
         doc["state_id"] = doc.pop("_id")
         return doc
 
-    def find_country_exact(self, country_name: str | None):
-
-        doc = self.country_collection.find_one(
-            {"name": country_name},
-            {"_id": 1},
-            collation={"locale": "en", "strength": 1}
-        )
-
-        return doc["_id"] if doc else None
-
-
-    def _find_country(self, query):
+    def find_country(self, query):
         doc = self.country_collection.find_one(
             query,
             {"_id": 1},
@@ -54,42 +43,42 @@ class WorldRepository:
     def lookup_location(self, country_name, state_name, city_name):
         match (bool(country_name), bool(state_name), bool(city_name)):
             case (True, True, True):
-                return self._find_city({
+                return self.find_city({
                     "name": city_name,
                     "state_name": state_name,
                     "country_name": country_name,
                 })
 
             case (True, True, False):
-                return self._find_state({
+                return self.find_state({
                     "name": state_name,
                     "country_name": country_name,
                 })
 
             case (True, False, True):
-                return self._find_city({
+                return self.find_city({
                     "name": city_name,
                     "country_name": country_name,
                 })
 
             case (False, True, True):
-                return self._find_city({
+                return self.find_city({
                     "name": city_name,
                     "state_name": state_name,
                 })
 
             case (True, False, False):
-                return self._find_country({
+                return self.find_country({
                     "name": country_name,
                 })
 
             case (False, True, False):
-                return self._find_state({
+                return self.find_state({
                     "name": state_name,
                 })
 
             case (False, False, True):
-                return self._find_city({
+                return self.find_city({
                     "name": city_name,
                 })
 
